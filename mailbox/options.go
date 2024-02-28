54e35af6fc33fbafe1970b4ac4a52a6b52abf8b2
Takeru KODAMA <zen.en.tonal@gmail.com>
2024-02-28 16:18:09 +0000

    [Add] mailboox options

--- mailbox/options.go
+++ mailbox/options.go
@@ -2,9 +2,21 @@ package mailbox
 
 import "github.com/zen-en-tonal/mtw/session"
 
+func WithFilters(filters ...session.Filter) Option {
+	return func(m *Mailbox) {
+		m.filters = append(m.filters, filters...)
+	}
+}
+
 func WithFilterSet(set FilterSet) Option {
 	return func(m *Mailbox) {
 		m.filters = append(m.filters, filterSet{set})
+	}
+}
+
+func WithHooks(hooks ...session.Hook) Option {
+	return func(m *Mailbox) {
+		m.hooks = append(m.hooks, hooks...)
 	}
 }
 
