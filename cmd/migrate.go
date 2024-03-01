fee161d841a37d813ac835e7f8284b8a4cd34749
Takeru KODAMA <zen.en.tonal@gmail.com>
2024-03-01 05:29:22 +0000

    migrations

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -3,15 +3,21 @@ package main
 import (
 	"log"
 
-	"github.com/jmoiron/sqlx"
+	"database/sql"
+
 	_ "github.com/lib/pq"
-	datastore "github.com/zen-en-tonal/mtw/database"
+	"github.com/zen-en-tonal/mtw/database"
 )
 
 func main() {
-	db, err := sqlx.Connect("postgres", "postgres://postgres:postgres@db:5432/postgres?sslmode=disable")
+	db, err := sql.Open(
+		"postgres",
+		"postgres://postgres:postgres@db:5432/postgres?sslmode=disable",
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.MustExec(datastore.Schema)
+	if err := database.Migrate(db); err != nil {
+		log.Fatal(err)
+	}
 }
