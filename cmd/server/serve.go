b796ddfed19beb4c397016fc9d93cf02599f9683
Takeru KODAMA <zen.en.tonal@gmail.com>
2024-03-03 05:54:20 +0000

    add cmds

---
+++ cmd/server/serve.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"log/slog"
+
+	"github.com/zen-en-tonal/mtw/database"
+	"github.com/zen-en-tonal/mtw/database/address"
+	"github.com/zen-en-tonal/mtw/database/webhook"
+	"github.com/zen-en-tonal/mtw/http"
+	"github.com/zen-en-tonal/mtw/mailbox"
+	"github.com/zen-en-tonal/mtw/smtp"
+	"github.com/zen-en-tonal/mtw/spam"
+)
+
+var (
+	domain string = ""
+)
+
+func init() {
+	flag.StringVar(&domain, "d", domain, "Domain")
+}
+
+func main() {
+	flag.Parse()
+
+	logger := slog.Default()
+
+	db, err := sql.Open(
+		"postgres",
+		"postgres://postgres:postgres@db:5432/postgres?sslmode=disable",
+	)
+	if err != nil {
+		logger.Error("failed to connect to db", "inner", err.Error())
+		return
+	}
+
+	if err := database.Migrate(db); err != nil {
+		logger.Warn("migration failure", "inner", err.Error())
+	}
+
+	smtp := smtp.New(
+		smtp.WithMailboxOptions(
+			mailbox.WithFilters(
+				spam.RcptMismatchFilter(),
+				address.Find(db),
+			),
+			mailbox.WithHookSet(webhook.NewFind(db)),
+		),
+		smtp.WithLogger(logger),
+	)
+	smtp.Addr = "0.0.0.0:25"
+	smtp.Domain = domain
+	smtp.AllowInsecureAuth = false
+
+	rest := http.NewWithDB(db, domain, logger)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func(ctx *context.Context) {
+		logger.Info("Listening and serving SMTP on 0.0.0.0:25")
+		if err := smtp.ListenAndServe(); err != nil {
+			logger.Error("smtp", "inner", err.Error())
+		}
+		cancel()
+	}(&ctx)
+	go func(ctx *context.Context) {
+		if err := rest.Run("0.0.0.0:8080"); err != nil {
+			logger.Error("rest", "inner", err.Error())
+		}
+		cancel()
+	}(&ctx)
+	<-ctx.Done()
+}
